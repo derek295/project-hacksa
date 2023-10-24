1a0fc06e691bb047b213035441c04769878320c7
redsmoke <48893695+derek295@users.noreply.github.com>
2023-10-23 19:07:33 -0500

    Script update
    script is updated to sync with upload button
    
    -derek

--- public/python/code.py
+++ public/python/code.py
@@ -1,5 +1,6 @@
 from cryptography.fernet import Fernet
 import os
+import cgi
 
 # Generate a random encryption key
 key = Fernet.generate_key()
@@ -12,34 +13,23 @@ ENCRYPTED_FILES_DIR = "encrypted_files"
 if not os.path.exists(ENCRYPTED_FILES_DIR):
     os.makedirs(ENCRYPTED_FILES_DIR)
 
-# Function to encrypt a file
-def encrypt_file(file_path, key):
-    with open(file_path, "rb") as file:
-        data = file.read()
-    encrypted_data = fernet.encrypt(data)
+# Get the uploaded file
+form = cgi.FieldStorage()
+uploaded_file = form['file']
+
+if uploaded_file:
+    # Save the uploaded file to a temporary location
+    original_filename = os.path.basename(uploaded_file.filename)
+    file_path = os.path.join(ENCRYPTED_FILES_DIR, f"encrypted_{original_filename}")
     
-    # Extract the original filename without the path
-    original_filename = os.path.basename(file_path)
+    with open(file_path, 'wb') as encrypted_file:
+        while True:
+            chunk = uploaded_file.file.read(8192)
+            if not chunk:
+                break
+            encrypted_chunk = fernet.encrypt(chunk)
+            encrypted_file.write(encrypted_chunk)
     
-    # Construct the encrypted file path
-    encrypted_file_path = os.path.join(ENCRYPTED_FILES_DIR, f"encrypted_{original_filename}")
-    
-    with open(encrypted_file_path, "wb") as encrypted_file:
-        encrypted_file.write(encrypted_data)
-    print(f"File '{original_filename}' encrypted and saved as '{os.path.basename(encrypted_file_path)}'.")
-
-# Main function
-def main():
-    while True:
-        file_path = input("Enter the path of the file to encrypt (or 'quit' to exit): ")
-
-        if file_path.lower() == "quit":
-            break
-
-        if os.path.isfile(file_path):
-            encrypt_file(file_path, key)
-        else:
-            print("File not found. Please enter a valid file path.")
-
-if __name__ == "__main__":
-    main()
+    print(f"File '{original_filename}' uploaded and encrypted successfully as '{os.path.basename(file_path)}'.")
+else:
+    print("File not found. Please select a file for upload.")
